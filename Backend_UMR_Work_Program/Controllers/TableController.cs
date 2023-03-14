e34751ba6e60e69bfdf140bbe389cdcd6a47c845
TargetAfrica <support@targetafrica.net>
2023-03-14 17:54:37 +0000

    Updated TableController.cs added addData

--- Backend_UMR_Work_Program/Controllers/TableController.cs
+++ Backend_UMR_Work_Program/Controllers/TableController.cs
@@ -77,7 +77,28 @@ namespace Backend_UMR_Work_Program.Controllers
             }
         }
 
+        [HttpGet("AddData")]
+        public async Task<string> AddSql()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection("Server=tcp:staging-servers.database.windows.net,1433;Initial Catalog=workprogram;Persist Security Info=False;User ID=serveradmin;Password=*123*brandonetech#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+                {
+                    await conn.OpenAsync();
+                    string query0 = $"ALTER TABLE dbo.ApplicationProccesses ADD ProcessStatus nvarchar(500) NULL;";
+                    SqlCommand cmd0 = new SqlCommand(query0, conn);
+                    var reader = await cmd0.ExecuteReaderAsync();
 
+                    return "Inserted";
+                }
+
+            }
+            catch (Exception e)
+            {
+                return null;
+
+            }
+        }
 
     }
 }
