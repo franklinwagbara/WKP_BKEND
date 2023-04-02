903bb6d5bd9db93cf3dab483b4159986db9e217e
Kenechukwu Frank <kenisank1@gmail.com>
2023-04-02 21:01:43 +0100

    WP-40

---
+++ Backend_UMR_Work_Program/DataModels/DECOMMISSIONING_ABANDONMENT.cs
@@ -0,0 +1,18 @@
+﻿namespace Backend_UMR_Work_Program.DataModels
+{
+    public class DECOMMISSIONING_ABANDONMENT
+    {
+        public int Id { get; set; }
+        public int? OmlId { get; set; }
+        public string CompanyEmail { get; set; }
+        public string WpYear { get; set; }
+        public int? FieldId { get; set; }
+        public string ApprovalStatus { get; set; }
+        public string ApprovalCostUsd { get; set; }
+        public string AnnualObigationUsd { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+    }
+}
