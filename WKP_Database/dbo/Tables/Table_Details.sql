9524e06b14a8c5ea4fe76c246f8679bd3b2cf99d
Franklin Godspower Wagbara <wagbarafranklin@yahoo.com>
2023-06-11 16:52:06 +0100

    modified table_detail table

--- WKP_Database/dbo/Tables/Table_Details.sql
+++ WKP_Database/dbo/Tables/Table_Details.sql
@@ -3,6 +3,9 @@
     [TableName]   VARCHAR (250) NULL,
     [TableSchema] VARCHAR (250) NULL,
     [SBU_ID]      VARCHAR (30)  NULL,
+    [Step] NVARCHAR(50) NULL, 
+    [Section] NVARCHAR(150) NULL, 
+    [SubSection] NVARCHAR(150) NULL, 
     CONSTRAINT [PK_Table_Details] PRIMARY KEY CLUSTERED ([TableId] ASC)
 );
 
