a7a38d2b5a6b07c9b725b630e3513fa7dd53de6c
emmanuel Okpukpan <emmaist23@gmail.com>
2023-06-13 10:51:56 +0100

    feat(legal arbitration): added year to model and updated db

--- Backend_UMR_Work_Program/Migrations/20230613084848_add-year-to-arbitration.cs
+++ Backend_UMR_Work_Program/Migrations/20230613084848_add-year-to-arbitration.cs
@@ -10,23 +10,23 @@ namespace Backend_UMR_Work_Program.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "Section",
-                table: "Table_Details",
-                type: "nvarchar(max)",
-                nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            //    name: "Section",
+            //    table: "Table_Details",
+            //    type: "nvarchar(max)",
+            //    nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "Step",
-                table: "Table_Details",
-                type: "nvarchar(max)",
-                nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            //    name: "Step",
+            //    table: "Table_Details",
+            //    type: "nvarchar(max)",
+            //    nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "SubSection",
-                table: "Table_Details",
-                type: "nvarchar(max)",
-                nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            //    name: "SubSection",
+            //    table: "Table_Details",
+            //    type: "nvarchar(max)",
+            //    nullable: true);
 
             migrationBuilder.AddColumn<string>(
                 name: "Year",
@@ -34,71 +34,71 @@ namespace Backend_UMR_Work_Program.Migrations
                 type: "nvarchar(max)",
                 nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "OSCPUploadName",
-                table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN",
-                type: "varchar(200)",
-                unicode: false,
-                maxLength: 200,
-                nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            //    name: "OSCPUploadName",
+            //    table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN",
+            //    type: "varchar(200)",
+            //    unicode: false,
+            //    maxLength: 200,
+            //    nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "OSCPUploadPath",
-                table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN",
-                type: "varchar(max)",
-                unicode: false,
-                nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            //    name: "OSCPUploadPath",
+            //    table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN",
+            //    type: "varchar(max)",
+            //    unicode: false,
+            //    nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "eMUploadName",
-                table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN",
-                type: "varchar(200)",
-                unicode: false,
-                maxLength: 200,
-                nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            //    name: "eMUploadName",
+            //    table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN",
+            //    type: "varchar(200)",
+            //    unicode: false,
+            //    maxLength: 200,
+            //    nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "eMUploadPath",
-                table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN",
-                type: "varchar(max)",
-                unicode: false,
-                nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            //    name: "eMUploadPath",
+            //    table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN",
+            //    type: "varchar(max)",
+            //    unicode: false,
+            //    nullable: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Section",
-                table: "Table_Details");
+            //migrationBuilder.DropColumn(
+            //    name: "Section",
+            //    table: "Table_Details");
 
-            migrationBuilder.DropColumn(
-                name: "Step",
-                table: "Table_Details");
+            //migrationBuilder.DropColumn(
+            //    name: "Step",
+            //    table: "Table_Details");
 
-            migrationBuilder.DropColumn(
-                name: "SubSection",
-                table: "Table_Details");
+            //migrationBuilder.DropColumn(
+            //    name: "SubSection",
+            //    table: "Table_Details");
 
             migrationBuilder.DropColumn(
                 name: "Year",
                 table: "LEGAL_ARBITRATION");
 
-            migrationBuilder.DropColumn(
-                name: "OSCPUploadName",
-                table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN");
+            //migrationBuilder.DropColumn(
+            //    name: "OSCPUploadName",
+            //    table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN");
 
-            migrationBuilder.DropColumn(
-                name: "OSCPUploadPath",
-                table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN");
+            //migrationBuilder.DropColumn(
+            //    name: "OSCPUploadPath",
+            //    table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN");
 
-            migrationBuilder.DropColumn(
-                name: "eMUploadName",
-                table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN");
+            //migrationBuilder.DropColumn(
+            //    name: "eMUploadName",
+            //    table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN");
 
-            migrationBuilder.DropColumn(
-                name: "eMUploadPath",
-                table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN");
+            //migrationBuilder.DropColumn(
+            //    name: "eMUploadPath",
+            //    table: "HSE_ENVIRONMENTAL_MANAGEMENT_PLAN");
         }
     }
 }
