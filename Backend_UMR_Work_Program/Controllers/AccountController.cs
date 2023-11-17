432c0b81228913f17001a78665063c32bd3746d4
TargetAfrica <support@targetafrica.net>
2023-11-17 12:34:25 +0000

    Updated AccountController.cs

--- Backend_UMR_Work_Program/Controllers/AccountController.cs
+++ Backend_UMR_Work_Program/Controllers/AccountController.cs
@@ -111,7 +111,7 @@ namespace Backend_UMR_Work_Program.Controllers
 			if (login.ResponseCode.Equals("00"))
 				return Redirect($"{_appSettings.LoginUrl}/login?id={login.Data}");
 
-			return Json(login);
+			return Ok(login);
 		}
 
 		[HttpPost("Authenticate")]
